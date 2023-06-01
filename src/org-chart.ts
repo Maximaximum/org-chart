aa402e39efa87506b8359fe590343de2e204d7b6
Petro Syvoshapka <syvoshapka@gmail.com>
2023-06-01 17:10:30 +0300

    Extract serialize-string.ts

--- src/org-chart.ts
+++ src/org-chart.ts
@@ -19,6 +19,7 @@ import {
 } from './d3-org-chart.types';
 import { BaseType, ZoomBehavior } from 'd3';
 import { getTextWidth } from './get-text-width';
+import { serializeString } from './serialize-string';
 
 const d3 = {
   selection,
@@ -2180,31 +2181,6 @@ export class OrgChart<Datum extends ConcreteDatum> {
       } else {
         location.replace(uri);
       }
-    }
-    // This function serializes SVG and sets all necessary attributes
-    function serializeString(svg: SVGElement) {
-      const xmlns = 'http://www.w3.org/2000/xmlns/';
-      const xlinkns = 'http://www.w3.org/1999/xlink';
-      const svgns = 'http://www.w3.org/2000/svg';
-      svg = svg.cloneNode(true) as SVGElement;
-      const fragment = window.location.href + '#';
-      const walker = document.createTreeWalker(
-        svg,
-        NodeFilter.SHOW_ELEMENT,
-        null
-      );
-      while (walker.nextNode()) {
-        for (const attr of (walker.currentNode as any).attributes) {
-          if (attr.value.includes(fragment)) {
-            attr.value = attr.value.replace(fragment, '#');
-          }
-        }
-      }
-      svg.setAttributeNS(xmlns, 'xmlns', svgns);
-      svg.setAttributeNS(xmlns, 'xmlns:xlink', xlinkns);
-      const serializer = new XMLSerializer();
-      const string = serializer.serializeToString(svg);
-      return string;
     }
   }
 }
